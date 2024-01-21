0535d77239d7a96013313657fb8d33c0cbeaf3f9
Chloe Bethel <chloe@9net.org>
2024-01-21 07:21:30 +0000

    drop cplusplus for adc.h

--- include/sam/adc.h
+++ include/sam/adc.h
@@ -2,17 +2,9 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#ifdef __cplusplus
 class ADCClass {
 public:
 	void init();
 	void select(unsigned int n);
 	uint16_t read();
 };
-
-extern "C" {
-#endif
-
-#ifdef __cplusplus
-}
-#endif
